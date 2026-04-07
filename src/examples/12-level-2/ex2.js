b3ace42d38210cc7a1c326f4662feb0e244d3489
hoiryu <comzkow@gmail.com>
2026-04-07 16:25:33 +0900

    서버 증설 횟수 복습

--- src/examples/12-level-2/ex2.js
+++ src/examples/12-level-2/ex2.js
@@ -10,15 +10,11 @@ const solution = (players, m, k) => {
 	const queue = [];
 
 	for (let i = 0; i < players.length; i++) {
-		while (queue.at(0) === i) queue.shift();
+		while (queue[0] <= i) queue.shift();
 
-		const current = players[i];
-
-		if (current < m) continue;
-
-		while (current >= (queue.length + 1) * m) {
-			answer++;
+		while ((queue.length + 1) * m <= players[i]) {
 			queue.push(i + k);
+			answer++;
 		}
 	}
 
@@ -27,15 +23,27 @@ const solution = (players, m, k) => {
 
 test(solution, [
 	{
-		input: [[0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5], 3, 5],
+		input: [
+			[0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5], //
+			3,
+			5,
+		],
 		expected: 7,
 	},
 	{
-		input: [[0, 0, 0, 10, 0, 12, 0, 15, 0, 1, 0, 1, 0, 0, 0, 5, 0, 0, 11, 0, 8, 0, 0, 0], 5, 1],
+		input: [
+			[0, 0, 0, 10, 0, 12, 0, 15, 0, 1, 0, 1, 0, 0, 0, 5, 0, 0, 11, 0, 8, 0, 0, 0], //
+			5,
+			1,
+		],
 		expected: 11,
 	},
 	{
-		input: [[0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 5, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1], 1, 1],
+		input: [
+			[0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 5, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1], //
+			1,
+			1,
+		],
 		expected: 12,
 	},
 ]);
