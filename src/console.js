78c70fc0f5e19d2e79a1cf9a05d35dddbf9453ca
hoiryu <comzkow@gmail.com>
2026-04-12 19:06:31 +0900

    퍼즐 게임 챌린지 복습

--- src/console.js
+++ src/console.js
@@ -1,19 +1,15 @@
 export const test = (f, cases) => {
-	Array.from({ length: 5 }, (_, idx) => console.log(`${idx} =======================================`));
+	Array.from({ length: 5 }, (_, idx) => console.log(`=======================================`));
 
 	cases.forEach(({ input, expected }, i) => {
 		const args = Array.isArray(input) ? input : [input];
 
 		const start = performance.now();
-
 		const result = f(...args);
-
 		const end = performance.now();
 
 		console.log(`[${i + 1}] 걸린 시간: ${(end - start).toFixed(3)}ms`);
-
 		const passed = JSON.stringify(result) === JSON.stringify(expected);
-
 		console.log(`[${i + 1}] ${passed ? '✅ PASS' : '❌ FAIL'}`);
 
 		if (!passed) {
--- src/examples/12-level-2/ex5.js
+++ src/examples/12-level-2/ex5.js
@@ -1,49 +1,51 @@
+import { test } from '../../console.js';
+
 /**
  * Two Pointers
  * 퍼즐 게임 챌린지
  * https://school.programmers.co.kr/learn/courses/30/lessons/340212
- *
- * ▣ 입력예제 1
- * [1, 5, 3], [2, 4, 7]
- * ▣ 출력예제 1
- * 30
- *
- * ▣ 입력예제 2
- * [1, 4, 4, 2], [6, 3, 8, 2]
- * ▣ 출력예제 2
- * 59
- *
- * ▣ 입력예제 3
- * [1, 328, 467, 209, 54], [2, 7, 1, 4, 3]
- * ▣ 출력예제 3
- * 1723
- *
- * ▣ 입력예제 4
- * [1, 99999, 100000, 99995], [9999, 9001, 9999, 9001]
- * ▣ 출력예제 4
- * 3456789012
  */
-export default function solution(diffs, times, limit) {
-	let answer = Infinity,
-		maximum = -Infinity;
-	for (const diff of diffs) maximum = Math.max(maximum, diff);
-	let p1 = 1,
-		p2 = maximum;
+const solution = (diffs, times, limit) => {
+	let answer = Infinity;
+	let pl = 1;
+	let pr = diffs.reduce((acc, curr) => Math.max(acc, curr), 0);
 
-	while (p1 <= p2) {
-		const level = Math.floor((p1 + p2) / 2);
+	while (pl <= pr) {
+		const pm = Math.floor((pl + pr) / 2);
 		let sum = 0;
+
 		for (let i = 0; i < diffs.length; i++) {
 			if (sum > limit) break;
-			if (level >= diffs[i]) sum += times[i];
-			else sum += (times[i] + times[i - 1]) * (diffs[i] - level) + times[i];
+
+			if (diffs[i] > pm) {
+				sum += (times[i] + times[i - 1]) * (diffs[i] - pm) + times[i];
+			} else sum += times[i];
 		}
-		if (sum > limit) p1 = level + 1;
-		else {
-			p2 = level - 1;
-			answer = Math.min(answer, level);
-		}
+
+		if (sum <= limit) {
+			pr = pm - 1;
+			answer = Math.min(answer, pm);
+		} else pl = pm + 1;
 	}
 
 	return answer;
-}
+};
+
+test(solution, [
+	{
+		input: [[1, 5, 3], [2, 4, 7], 30],
+		expected: 3,
+	},
+	{
+		input: [[1, 4, 4, 2], [6, 3, 8, 2], 59],
+		expected: 2,
+	},
+	{
+		input: [[1, 328, 467, 209, 54], [2, 7, 1, 4, 3], 1723],
+		expected: 294,
+	},
+	{
+		input: [[1, 99999, 100000, 99995], [9999, 9001, 9999, 9001], 3456789012],
+		expected: 39354,
+	},
+]);
